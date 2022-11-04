b829500b3c841b34b8062811cb3952b8bbdf1227
ImranAhmedGS <108127760+ImranAhmedGS@users.noreply.github.com>
2022-11-04 19:17:18 +0530

    Add files via upload

---
+++ xmlconvertertojson.py
@@ -0,0 +1,23 @@
+import json
+import xmltodict
+import sys
+import logging
+
+
+def run(inpfilename):
+	filecontent=open(inpfilename).read()
+	
+xmlparser=xmltodict.parse(filecontent)
+
+jso = json.dumps(xmlparser)
+jso = jso.replace('@',").replace('#',")
+
+with open('outputfile1.ndl','w') as fileto:
+	fileto.write(jso)
+
+# Just to rn this command
+# bq load -autodetect=true -source_format=NEWLINE_DELIMITED_JSON ctestmigration.errortable.outputfile.ndl
+
+if __name__=='__main__':
+	filename = sys.argv[1]
+	run(filename)
